8ae6dd51855553b302cf6d8c5e0d313996870ebb
kotonetto <kotonekko0925@gmail.com>
2025-06-22 00:12:47 +0900

    HP上限３個

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 // ゲーム設定
-export const MAX_HP = 5;
+export const MAX_HP = 3;
 
 // Google認証設定
 export const GOOGLE_CLIENT_ID = "527828076351-j8ajmuudb77dkmt7jrd4j4l791fpbvsh.apps.googleusercontent.com"; 
